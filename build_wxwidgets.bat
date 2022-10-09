f93de8d6708c5206486c0bb3d9e4d677cb87352b
ihedvall <92839244+ihedvall@users.noreply.github.com>
2022-10-09 12:08:29 +0200

    Added cmake file that fix cmake version 3.23 bug regarding wxwidgets

--- build_wxwidgets.bat
+++ build_wxwidgets.bat
@@ -1,8 +1,8 @@
-rem Usage build_wxwidgets <SourceDir> <DestDir> <BuildDir> <Tool>. 
-rem <SourceDir> (%1) Full path to the wxwidgets repository.
-rem <DestDir> (%2) Full path to the destination directory (Example: k:/wxwidgets/master.
-rem <BuildDir> (%3) Full path to a temporary directory where the build is done. The directory should exist.
-rem <Tool> (%4) Choose gcc or msvc. 
+rem Usage build_wxwidgets SourceDir DestDir BuildDir Tool.
+rem SourceDir (%1) Full path to the wxwidgets repository.
+rem DestDir (%2) Full path to the destination directory (Example: k:/wxwidgets/master.
+rem BuildDir (%3) Full path to a temporary directory where the build is done.
+rem Tool (%4) Choose gcc or msvc.
  
 rem The build directory should exist. The build is done in a sub-directory.
 rem You must use backslash in directory names (K:\foo not k:/foo)
@@ -12,26 +12,57 @@ set DEST_DIR=%2
 set BUILD_DIR=%3
 set BUILD_TOOL=%4
 
+if [%BUILD_TOOL%] == [] set BUILD_TOOL=msvc
+echo Build Tool: %BUILD_TOOL%
+
 set GENERATOR="Visual Studio 17 2022"
 if %BUILD_TOOL%==gcc set GENERATOR="MinGW Makefiles"
+echo Generator: %GENERATOR%
 
 set ARCH=
-if %BUILD_TOOL%==msvc set ARCH=-Ax64
+if %BUILD_TOOL%==msvc set ARCH=-A x64
+echo Architecture: %ARCH%
 
-rmdir "%BUILD_DIR%\wxwidgets" /s /q
-mkdir "%BUILD_DIR%\wxwidgets"
-mkdir "%DEST_DIR%"
+if [%SOURCE_DIR%] == [] (
+    echo Error: Source directory must be supplied.
+    exit /b 1
+)
+
+if not exist "%SOURCE_DIR%" (
+    echo Error: Source directory must exist.
+    exit /b 1
+)
+
+if [%DEST_DIR%] == [] (
+    echo Error: Destination directory must be supplied.
+    exit /b 1
+)
+
+if not exist "%DEST_DIR%" (
+    mkdir "%DEST_DIR%"
+    echo Making destination directory: %DEST_DIR%
+)
+
+if [%BUILD_DIR%] == [] set BUILD_DIR=%TEMP%\wxwidgets
+echo Using temporary directory: %BUILD_DIR%
+
+if not exist "%BUILD_DIR%" (
+    mkdir "%BUILD_DIR%"
+    echo Making temporary directory  %BUILD_DIR%
+)
 
 for %%C in ("Debug" "Release") do (
 echo Generate WXWIDGETS build environment
-cmake -G %GENERATOR% -S "%SOURCE_DIR%" -B "%BUILD_DIR%\wxwidgets" -D CMAKE_BUILD_TYPE=%%C %ARCH% -D wxBUILD_SHARED=OFF -D wxBUILD_UNICODE=ON -D wxBUILD_DEMOS=ON -D wxBUILD_SAMPLES=ALL 
+cmake -G %GENERATOR% -S "%SOURCE_DIR%" -B "%BUILD_DIR%" -D CMAKE_INSTALL_PREFIX="%DEST_DIR%" ^
+      -D CMAKE_BUILD_TYPE=%%C %ARCH% -D wxBUILD_SHARED=OFF -D wxBUILD_DEMOS=OFF ^
+      -D wxBUILD_SAMPLES=OFF -D wxBUILD_INSTALL=ON
 
 echo Build WXWIDGETS library
-cmake --build "%BUILD_DIR%\wxwidgets"  --clean-first --parallel 24 --config %%C
+cmake --build "%BUILD_DIR%"  --clean-first --parallel 24 --config %%C
 
 echo Install WXWIDGETS library 
-cmake  --install "%BUILD_DIR%\wxwidgets" --config %%C --prefix "%DEST_DIR%"
+cmake  --install "%BUILD_DIR%" --config %%C --prefix "%DEST_DIR%"
 )
 
 echo Removing WXWIDGETS build directory
-rmdir "%BUILD_DIR%\wxwidgets" /s /q
+rmdir "%BUILD_DIR%" /s /q
